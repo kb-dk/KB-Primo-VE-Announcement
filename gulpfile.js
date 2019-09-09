91b42c432913a6c647698384a6c38942052247f1
jara <jara@kb.dk>
2019-09-09 15:09:56 +0200

    browserified dist file that works in primo

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,28 @@
-var gulp = require('gulp');
-var browserify = require('browserify');
-var babelify = require('babelify');
-var source = require('vinyl-source-stream');
+"use strict";
 
-gulp.task('build', function () {
-    return browserify({entries: 'src/main.js', debug: true})
-        .transform(babelify)
-        .bundle()
-        .pipe(source('index.js'))
-        .pipe(gulp.dest('dist'));
+const gulp = require("gulp");
+const browserify = require("browserify");
+const babelify = require("babelify");
+const source = require("vinyl-source-stream");
+const buffer = require("vinyl-buffer");
+const uglify = require('gulp-uglify');
+
+gulp.task("build", function() {
+  return browserify({
+    debug: true,
+    entries: "src/main.js",
+    paths: ["/node_modules"]
+  })
+    .transform("babelify", {
+      presets: ["es2015"],
+      plugins: ["transform-html-import-to-string"],
+      sourceMaps: true
+    })
+    .bundle()
+    .pipe(source("index.js"))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"));
 });
 
-gulp.task('default', gulp.series('build'));
+gulp.task("default", gulp.series("build"));
